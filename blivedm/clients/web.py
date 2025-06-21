cc6b6a184232c54890ddaa44febbd2a5dfb73d53
Raven95676 <Raven95676@gmail.com>
2025-06-21 21:59:33 +0800

    style: format code

--- blivedm/clients/web.py
+++ blivedm/clients/web.py
@@ -20,44 +20,48 @@ MIXIN_KEY_ENC_TAB = [
     61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
     36, 20, 34, 44, 52
 ]
-NAV_URL = 'https://api.bilibili.com/x/web-interface/nav'
+NAV_URL = "https://api.bilibili.com/x/web-interface/nav"
+
 
 def _get_mixin_key(orig: str):
     """对 imgKey 和 subKey 进行字符顺序打乱编码"""
-    return reduce(lambda s, i: s + orig[i], MIXIN_KEY_ENC_TAB, '')[:32]
+    return reduce(lambda s, i: s + orig[i], MIXIN_KEY_ENC_TAB, "")[:32]
+
 
 async def _get_wbi_keys_async(session) -> tuple[str, str]:
     """获取最新的 img_key 和 sub_key (异步版本)"""
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        'Referer': 'https://www.bilibili.com/'
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "Referer": "https://www.bilibili.com/",
     }
     async with session.get(NAV_URL, headers=headers) as resp:
         resp.raise_for_status()
         json_content = await resp.json()
 
-    img_url: str = json_content['data']['wbi_img']['img_url']
-    sub_url: str = json_content['data']['wbi_img']['sub_url']
-    img_key = img_url.rsplit('/', 1)[1].split('.')[0]
-    sub_key = sub_url.rsplit('/', 1)[1].split('.')[0]
+    img_url: str = json_content["data"]["wbi_img"]["img_url"]
+    sub_url: str = json_content["data"]["wbi_img"]["sub_url"]
+    img_key = img_url.rsplit("/", 1)[1].split(".")[0]
+    sub_key = sub_url.rsplit("/", 1)[1].split(".")[0]
     return img_key, sub_key
+
 
 async def _enc_wbi_async(session, params: dict):
     """为请求参数进行 wbi 签名 (异步版本)"""
     img_key, sub_key = await _get_wbi_keys_async(session)
     mixin_key = _get_mixin_key(img_key + sub_key)
     curr_time = round(time.time())
-    params['wts'] = curr_time                                   # 添加 wts 字段
-    params = dict(sorted(params.items()))                       # 按照 key 重排参数
+    params["wts"] = curr_time  # 添加 wts 字段
+    params = dict(sorted(params.items()))  # 按照 key 重排参数
     # 过滤 value 中的 "!'()*" 字符
     params = {
-        k: ''.join(filter(lambda char: char not in "!'()*", str(v)))
+        k: "".join(filter(lambda char: char not in "!'()*", str(v)))
         for k, v in params.items()
     }
-    query = urllib.parse.urlencode(params)                      # 序列化参数
-    wbi_sign = md5((query + mixin_key).encode()).hexdigest()     # 计算 w_rid
-    params['w_rid'] = wbi_sign
+    query = urllib.parse.urlencode(params)  # 序列化参数
+    wbi_sign = md5((query + mixin_key).encode()).hexdigest()  # 计算 w_rid
+    params["w_rid"] = wbi_sign
     return params
+
 
 UID_INIT_URL = "https://api.bilibili.com/x/web-interface/nav"
 BUVID_INIT_URL = "https://www.bilibili.com/"
